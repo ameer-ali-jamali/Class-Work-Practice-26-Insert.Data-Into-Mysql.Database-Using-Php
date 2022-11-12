14815c05a0f2388e673f181f672d2a81ac1fca60
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-11-12 15:41:52 +0500

    insert data query

--- action_page.php
+++ action_page.php
@@ -15,11 +15,31 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
+$select = "SELECT * FROM tab ";
 
 $update = "UPDATE `tab` SET `Name`='Anwar',`Email`='anwar@gmail.com',`Password`='john22' WHERE Id=1";
 $delete = " DELETE FROM tab WHERE Name='Ameer_ali' and Email='email@j.com' and Password='amjad' ";
 $insert_data = "INSERT INTO `tab`(`Name`, `Email`, `Password`) VALUES ('$name','$email','$password')";
-$result = mysqli_query($conn, $delete);
 
+$result = mysqli_query($conn, $insert_data);
+echo "<hr>";
+if ($result) {
+    echo "This is User Name = " . " " .  $name . "<br>" . "This is User Email = " . $email . "<br>" . "This is User Password = " . $password . "<br>" . "<hr>" . "Data Submited Sucessfully";
+} else {
+    echo "New User Data Not Submited";
+}
+
+// while ($select == "") {
+//     echo "<table>";
+//     echo "<tr>";
+//     echo "<th>Name</th>";
+//     echo "<th>Email</th>";
+//     echo " </tr>";
+//     echo "<tr>";
+//     echo "<td>$name</td>";
+//     echo "<td>$email</td>";
+//     echo "</tr>";
+//     echo "</table>";
+// }
 
 ?>
