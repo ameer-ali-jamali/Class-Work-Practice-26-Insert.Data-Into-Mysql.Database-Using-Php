af7a9864ea42a6133e6c0bd0159a0e9bb1681ec2
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-11-12 22:22:05 +0500

    connection page

--- action_page.php
+++ action_page.php
@@ -1,45 +1,26 @@
 <?php
-
-try {
-    $conn = new mysqli("localhost", "root", "", "std");
-} catch (Exception $ex) {
-    echo $ex . "This is error";
-    die("Connection failed");
-}
-echo "Database Connected Sucsessfully";
-
+include_once "connection.php";
 ?>
+
 <?php
 
 $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$select = "SELECT * FROM tab ";
-
+// $select = "SELECT * FROM tab ";
+$insert = "INSERT INTO `tab`(`Name`, `Email`, `Password`) VALUES ('$name','$email','$password')";
 $update = "UPDATE `tab` SET `Name`='Anwar',`Email`='anwar@gmail.com',`Password`='john22' WHERE Id=1";
 $delete = " DELETE FROM tab WHERE Name='Ameer_ali' and Email='email@j.com' and Password='amjad' ";
-$insert_data = "INSERT INTO `tab`(`Name`, `Email`, `Password`) VALUES ('$name','$email','$password')";
 
-$result = mysqli_query($conn, $insert_data);
+$result = mysqli_query($conn, $insert);
 echo "<hr>";
-if ($result) {
-    echo "This is User Name = " . " " .  $name . "<br>" . "This is User Email = " . $email . "<br>" . "This is User Password = " . $password . "<br>" . "<hr>" . "Data Submited Sucessfully";
-} else {
-    echo "New User Data Not Submited";
-}
 
-// while ($select == "") {
-//     echo "<table>";
-//     echo "<tr>";
-//     echo "<th>Name</th>";
-//     echo "<th>Email</th>";
-//     echo " </tr>";
-//     echo "<tr>";
-//     echo "<td>$name</td>";
-//     echo "<td>$email</td>";
-//     echo "</tr>";
-//     echo "</table>";
+
+// while ($data = mysqli_fetch_assoc($result)) {
+//     echo "<hr>" . $data['Id'], $data['Name'] . $data['Email'] . $data['Password'];
 // }
+
+
 
 ?>
